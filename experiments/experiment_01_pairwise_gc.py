5907d451e57ecb5d8a4aaab261eb37f22546adcb
hlpmalshan <hlpmalshan@gmail.com>
2026-06-18 14:28:51 -0400

    Updated experiment 01

--- experiments/experiment_01_pairwise_gc.py
+++ experiments/experiment_01_pairwise_gc.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 from src.simulation.var_generator import generate_var_data
+from src.gc.pairwise_gc import pairwise_gc
 
 A1 = np.array([
-    [0.5, 0.4],
-    [0.5, 0.8]
+    [0.5, 0.8],
+    [0.0, 0.8]
 ])
 
 Sigma = np.eye(2)
@@ -15,4 +16,20 @@ data = generate_var_data(
     n_samples=5000
 )
 
-print(data.shape)
+print("Pairwise Y->X",
+    pairwise_gc(
+        data,
+        source=1,
+        target=0,
+        order=1
+    )
+)
+
+print("Pairwise X->Y",
+    pairwise_gc(
+        data,
+        source=0,
+        target=1,
+        order=1
+    )
+)
